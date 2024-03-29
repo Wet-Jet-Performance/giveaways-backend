1e9d561a115ca95c50845a55d1667ffcd7e3a91c
kendall holmes <121322044+kendalliscoding@users.noreply.github.com>
2024-03-29 12:48:51 -0700

    add get one by id and post two tests

--- tests/test_giveaways.py
+++ tests/test_giveaways.py
@@ -20,3 +20,64 @@ def test_get_all_giveaways_returns_list_of_giveaways(client, two_giveaways):
          "start_date": "Sun, 21 Apr 2024 00:00:00 GMT",
          "end_date": "Tue, 23 Apr 2024 00:00:00 GMT"}
     ]
+
+def test_create_two_giveaways(client):
+    response1 = client.post("/giveaways", json={
+        "name": "New Giveaway 1",
+        "start_date": "July 1, 2024",
+        "end_date": "July 7, 2024"
+    })
+    response_body1 = response1.get_json()
+
+    response2 = client.post("/giveaways", json={
+        "name": "New Giveaway 2",
+        "start_date": "August 1, 2024",
+        "end_date": "August 7, 2024"
+    })
+    response_body2 = response2.get_json()
+
+    assert response1.status_code == 201
+    assert "success" in response_body1["msg"].lower()
+    assert "1" in response_body1["msg"]
+
+    assert response2.status_code == 201
+    assert "success" in response_body2["msg"].lower()
+    assert "2" in response_body2["msg"]
+
+    #ensure new data is in database
+    response3 = client.get("/giveaways")
+    response_body3 = response3.get_json()
+
+    assert response3.status_code == 200
+    assert response_body3 == [
+        {"id": 1,
+         "name": "New Giveaway 1",
+         "start_date": "Mon, 01 Jul 2024 00:00:00 GMT",
+         "end_date": "Sun, 07 Jul 2024 00:00:00 GMT"},
+        {"id": 2,
+         "name": "New Giveaway 2",
+         "start_date": "Thu, 01 Aug 2024 00:00:00 GMT",
+         "end_date": "Wed, 07 Aug 2024 00:00:00 GMT"}
+    ]
+
+def test_get_giveaway_by_id_returns_correct_giveaway(client, two_giveaways):
+    response1 = client.get("/giveaways/1")
+    response_body1 = response1.get_json()
+
+    response2 = client.get("/giveaways/2")
+    response_body2 = response2.get_json()
+
+    assert response1.status_code == 200
+    assert response_body1 == {
+        "id": 1,
+        "name": "Giveaway 1",
+        "start_date": "Thu, 28 Mar 2024 00:00:00 GMT",
+        "end_date": "Fri, 29 Mar 2024 00:00:00 GMT"
+        }
+    assert response2.status_code == 200
+    assert response_body2 == {
+        "id": 2,
+        "name": "Giveaway 2",
+        "start_date": "Sun, 21 Apr 2024 00:00:00 GMT",
+        "end_date": "Tue, 23 Apr 2024 00:00:00 GMT"
+        }
--- tests/test_participants.py
+++ tests/test_participants.py
@@ -20,3 +20,65 @@ def test_get_all_participants_returns_list_of_participants(client, two_participa
          "phone_number": "(123)456-7891",
          "email": "participant2@email.com"}
     ]
+
+def test_create_two_participants(client):
+    response1 = client.post("/participants", json={
+        "name": "New Participant 1",
+        "phone_number": "(123)456-7890",
+        "email": "newparticipant1@email.com"
+    })
+    response_body1 = response1.get_json()
+
+    response2 = client.post("/participants", json={
+        "name": "New Participant 2",
+        "phone_number": "(123)456-7891",
+        "email": "newparticipant2@email.com"
+    })
+    response_body2 = response2.get_json()
+
+    assert response1.status_code == 201
+    assert "success" in response_body1["msg"].lower()
+    assert "1" in response_body1["msg"]
+
+    assert response2.status_code == 201
+    assert "success" in response_body2["msg"].lower()
+    assert "2" in response_body2["msg"]
+
+    #ensure new data is in database
+    response3 = client.get("/participants")
+    response_body3 = response3.get_json()
+
+    assert response3.status_code == 200
+    assert response_body3 == [
+        {"id": 1,
+         "name": "New Participant 1",
+         "phone_number": "(123)456-7890",
+         "email": "newparticipant1@email.com"},
+        {"id": 2,
+         "name": "New Participant 2",
+         "phone_number": "(123)456-7891",
+         "email": "newparticipant2@email.com"}
+    ]
+
+def test_get_participant_by_id_returns_correct_participant(client, two_participants):
+    response1 = client.get("/participants/1")
+    response_body1 = response1.get_json()
+
+    assert response1.status_code == 200
+    assert response_body1 == {
+        "id": 1,
+        "name": "Participant 1",
+        "phone_number": "(123)456-7890",
+        "email": "participant1@email.com"
+    }
+
+    response2 = client.get("/participants/2")
+    response_body2 = response2.get_json()
+
+    assert response2.status_code == 200
+    assert response_body2 == {
+        "id": 2,
+        "name": "Participant 2",
+        "phone_number": "(123)456-7891",
+        "email": "participant2@email.com"
+    }
