2c4d602dee29e197173aa63acf2bd3fae7365835
kendall holmes <121322044+kendalliscoding@users.noreply.github.com>
2024-04-03 14:11:45 -0700

    test post requests for winners and tickets

--- tests/test_tickets.py
+++ tests/test_tickets.py
@@ -18,3 +18,38 @@ def test_get_all_tickets_returns_list_of_tickets(client, two_giveaways, two_part
          "giveaway_id": 1,
          "participant_id": 2}
     ]
+
+def test_create_two_tickets(client, two_giveaways, two_participants):
+    response1 = client.post("/tickets", json={
+        "giveaway_id": 1,
+        "participant_id": 1
+    })
+    response_body1 = response1.get_json()
+
+    response2 = client.post("/tickets", json={
+        "giveaway_id": 1,
+        "participant_id": 2
+    })
+    response_body2 = response2.get_json()
+
+    assert response1.status_code == 201
+    assert "success" in response_body1["msg"].lower()
+    assert "1" in response_body1["msg"]
+
+    assert response2.status_code == 201
+    assert "success" in response_body2["msg"].lower()
+    assert "2" in response_body2["msg"]
+
+    # ensure new ticket in database
+    response3 = client.get("/tickets")
+    response_body3 = response3.get_json()
+
+    assert response3.status_code == 200
+    assert response_body3 == [
+        {"id": 1,
+         "giveaway_id": 1,
+         "participant_id": 1},
+        {"id": 2,
+         "giveaway_id": 1,
+         "participant_id": 2}
+    ]
