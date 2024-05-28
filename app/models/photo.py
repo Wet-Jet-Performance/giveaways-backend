3b1f53ec425f8d50636aff8d7eb8119b5bcbd171
kendall holmes <121322044+kendalliscoding@users.noreply.github.com>
2024-05-28 12:19:44 -0700

    rollback add photo

--- app/models/photo.py
+++
@@ -1,9 +0,0 @@
-from app import db
-from .participant import Participant
-from .giveaway import Giveaway
-
-class Photo(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    filename = db.Column(db.String, nullable=False)
-    giveaway_id = db.Column(db.Integer, db.ForeignKey(Giveaway.id))
-    giveaway = db.relationship('Giveaway', back_populates="photos")
--- app/routes/giveaways.py
+++ app/routes/giveaways.py
@@ -1,19 +1,14 @@
 from flask import Blueprint, request, current_app
 from app import db
 from app.models.giveaway import Giveaway
-from app.models.photo import Photo
-from base64 import encodebytes
 from datetime import date
-from PIL import Image
-from werkzeug.utils import secure_filename
-import io
 import os
 
 giveaways_bp = Blueprint("giveaways", __name__, url_prefix="/giveaways")
 
 @giveaways_bp.route('', methods=['POST'])
 def create_giveaway():
-    request_body = dict(request.form)
+    request_body = request.get_json()
 
     new_giveaway = Giveaway(name=request_body["name"],
                             start_date=request_body["start_date"],
@@ -22,13 +17,6 @@ def create_giveaway():
     
     
     db.session.add(new_giveaway)
-    db.session.commit()
-
-    for photo in request.files.getlist('photos[]'):
-        filename = secure_filename(photo.filename)
-        photo.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
-        new_photo = Photo(filename=filename, giveaway_id=new_giveaway.id)
-        db.session.add(new_photo)
     db.session.commit()
 
     return {"msg": "Successfully created new Giveaway",
@@ -51,8 +39,7 @@ def get_giveaways():
                 "giveaway_id": winner.giveaway_id,
                 "participant_id": winner.participant_id,
                 "winning_ticket_id": winner.winning_ticket_id
-            } for winner in giveaway.winners],
-            "photos": [encode_photo(photo) for photo in giveaway.photos]
+            } for winner in giveaway.winners]
         })
     return return_giveaways, 200
 
@@ -131,20 +118,7 @@ def delete_giveaway(giveaway_id):
     for ticket in giveaway.tickets:
         db.session.delete(ticket)
 
-    for photo in giveaway.photos:
-        os.remove(os.path.join(current_app.config['UPLOAD_FOLDER'], photo.filename))
-        db.session.delete(photo)
-
     db.session.delete(giveaway)
     db.session.commit()
 
     return {"msg":f"Successfully deleted Giveaway with id {giveaway_id}"}, 200
-
-def encode_photo(photo):
-    image_path = os.path.join(current_app.config['UPLOAD_FOLDER'], photo.filename)
-    file_extension = photo.filename.rsplit('.', 1)[1].upper()
-    pil_img = Image.open(image_path, mode='r') # reads the PIL image
-    byte_arr = io.BytesIO()
-    pil_img.save(byte_arr, format=file_extension) # convert the PIL image to byte array
-    encoded_img = encodebytes(byte_arr.getvalue()).decode('ascii') # encode as base64
-    return (encoded_img, file_extension)
