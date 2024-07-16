a72545f72a9c6e9affe0234153611df6364c082f
Satyam Singh <satyamsingh5august@gmail.com>
2024-07-16 23:58:30 +0530

    100k tickets fetching

--- app/routes/tickets.py
+++ app/routes/tickets.py
@@ -51,12 +51,12 @@ def send_email():
 # Only fetches 10000 tickets due to timeout and frontend can't handle more than 10000 tickets
 @tickets_bp.route("", methods=["GET"])
 def get_tickets():
-    # tickets = db.session.query(Ticket)\
-    #                     .order_by(Ticket.id.desc())\
-    #                     .limit(2000)\
-    #                     .all()
+    tickets = db.session.query(Ticket)\
+                        .order_by(Ticket.id.desc())\
+                        .limit(100000)\
+                        .all()
     
-    tickets = db.session.query(Ticket).order_by(Ticket.id.desc()).all()
+    # tickets = db.session.query(Ticket).order_by(Ticket.id.desc()).all()
 
 
     return_tickets = []
